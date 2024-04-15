199e931f4da11000360988feee3c64d7a7c122dc
Valya Ivanenko <valyshkagor@mail.ru>
2024-04-15 20:33:24 +0700

    popytka two

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <windows.h>
-#include <clocale>
 #include <ctime>
 
 
@@ -8,20 +7,21 @@ using namespace std;
 
 int main()
 {
-    setlocale(LC_CTYPE, "rus");
     srand(time(NULL));
 
-    int n=0, a=0, b=0;
+    int n=0, a=0, b=0, c=0;
 
-    cout << "Введите количество билетов:\n";
+    cout << "Vvedite kolichestvo biletov:\n";
     cin >> n;
 
-    cout << "Напишите любое число:\n";
-    cin >> a;
+   while(true) {
+        system ("cls");
+        cout << "Napishite lyboe chislo:\n";
+        cin >> a;
 
     b=1+rand()%n;
-    cout << "Ваш билет:" << b << endl;
-
+    cout << "Vash bilet:" << b << endl;
+    system ("pause");}
 
     return 0;
 }
